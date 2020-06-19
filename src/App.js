aa03066b195c6c4c0eb1ee9fed2a5f3d01301618
Eric Dalmau <dalmaueric29@gmail.com>
2020-06-18 17:28:53 -0700

    got incidents to display on screen using react

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios'
 import './App.css';
 
-const App = () => {
-  const [incidents, setIncidents] = useState([])
-
-  useEffect(()=>{
-     axios.get('/api/now/table/incident?sysparm_limit=10')
-          .then(res => {
-            setIncidents(res.data.result)
-          })
-  },[])
-
-  const Incidents = () => {
-    return (
-      <ul>
-        {incidents.map( (i)=>(<li key={i.sys_id}>{i.number}: {i.short_description}</li>)) }
-      </ul>
-    )
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      incidentReport: [],
+    }
   }
 
-  return (
-    <div>
-      <h2>Hello, ServiceNow!</h2>
-      <Incidents/>
-    </div>
-  )
+  componentDidMount() {
+    axios.get('/api/now/table/incident')
+      .then(res => res.data.result)
+      .then(incidentReport => this.setState({ incidentReport }))
+
+  }
+  render() {
+    const Incidents = () => {
+      return (
+        <div>
+          {this.state.incidentReport.map((i) =>
+            (<div key={i.sys_id} className="card darken-1">
+              <div className="card-content">
+                <span className="card-title" style={{ color: 'green' }}>
+                  {i.number}
+                </span>
+                <p className="description">{i.short_description}</p>
+                <button style={{ marginTop: 10 }} className="ui basic button">View Incident</button>
+              </div>
+            </div>))}
+        </div>
+      )
+    }
+    return (
+      <div className='container'>
+        <nav>
+          <div className='nav-wrapper center aligned blue' style={{ marginBottom: 40, fontSize:40 }} >
+            Open Incident Report
+        </div>
+        </nav>
+        <div style={{marginTop:40}}>
+        <Incidents />
+        </div>
+      </div>
+    )
+  }
 }
+
 
 export default App;
